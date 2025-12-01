a0230e70f5e04101dee17ae45a0948cc89a98610
aradhanapagadala <aradhana.pagadala@outlook.com>
2025-12-01 17:01:49 -0600

    Add files via upload

---
+++ treediameter.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int height(TreeNode* root) {
+  
+        if (root == nullptr)
+        return 0;
+
+        return 1 + max(height(root->left), height(root->right));
+    }      
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            int lheight = height(root->left);
+            int rheight = height(root->right);
+
+            int ldiameter = diameterOfBinaryTree(root->left);
+            int rdiameter = diameterOfBinaryTree(root->right);
+
+            return max({lheight + rheight, ldiameter, rdiameter});
+        }
+    }
+};
